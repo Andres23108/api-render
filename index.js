4f929359d9e4a44945770c194207722756ce738a
Andres23108 <99457832+Andres23108@users.noreply.github.com>
2022-08-12 13:12:23 -0400

    add sizes & add products

--- index.js
+++ index.js
@@ -6,115 +6,157 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const producto = [
-    {
-      id: 1,
-      name: "Camiseta Puma",
-      colors: ["red", "gray", "black", "blue "],
-      price: 180,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 2,
-      name: "Sudadera Nike",
-      colors: ["red", "gray", "black", "blue "],
-      price: 100,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 3,
-      name: "Polera City",
-      colors: ["red", "gray", "black", "blue "],
-      price: 150,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 4,
-      name: "Short Adidas",
-      price: 160,
-      colors: ["red", "gray", "black", "blue "],
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 5,
-      name: "Camiseta Puma",
-      colors: ["red", "gray", "black", "blue "],
-      price: 180,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 6,
-      name: "Sudadera Nike",
-      colors: ["red", "gray", "black", "blue "],
-      price: 100,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 7,
-      name: "Polera City",
-      colors: ["red", "gray", "black", "blue "],
-      price: 150,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 8,
-      name: "Short Adidas",
-      colors: ["red", "gray", "black", "blue "],
-      price: 160,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 9,
-      name: "Camiseta Puma",
-      colors: ["red", "gray", "black", "blue "],
-      price: 180,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 10,
-      name: "Sudadera Nike",
-      colors: ["red", "gray", "black", "blue "],
-      price: 100,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 11,
-      name: "Polera City",
-      colors: ["red", "gray", "black", "blue "],
-      price: 150,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-    {
-      id: 12,
-      name: "Short Adidas",
-      colors: ["red", "gray", "black", "blue "],
-      price: 160,
-      quantity: 1,
-      imageSrc:
-        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-    },
-  ];
+  {
+    id: 1,
+    name: "Hoodie Anime",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Sudaderas",
+    price: 180,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1660249137/e-commerce-x/hoddie_anime_swzi9p.webp",
+  },
+  {
+    id: 2,
+    name: "Sudadera Nike",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Sudaderas",
+    price: 100,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1660249137/e-commerce-x/hoddie_anime_swzi9p.webp",
+  },
+  {
+    id: 3,
+    name: "Hoodie Anime",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Sudaderas",
+    price: 150,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1660249137/e-commerce-x/hoddie_anime_swzi9p.webp",
+  },
+  {
+    id: 4,
+    name: "Short Adidas",
+    price: 160,
+    sizes: ["S", "M", "L", "XL"],
+    category: "Shorts",
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
+  },
+  {
+    id: 5,
+    name: "Camiseta Puma",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Camisetas",
+    price: 180,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
+  },
+  {
+    id: 6,
+    name: "Sudadera Nike",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Sudaderas",
+    price: 100,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
+  },
+  {
+    id: 7,
+    name: "Pantalón Azul",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Pantalones",
+    price: 150,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+  },
+  {
+    id: 8,
+    name: "Short Adidas",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Shorts",
+    price: 160,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
+  },
+  {
+    id: 9,
+    name: "Camiseta Puma",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Camisetas",
+    price: 180,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+  },
+  {
+    id: 10,
+    name: "Sudadera Nike",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Sudaderas",
+    price: 100,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+  },
+  {
+    id: 11,
+    name: "Tennis Nike",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Tennis",
+    price: 150,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+  },
+  {
+    id: 12,
+    name: "Short Adidas",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Shorts",
+    price: 160,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
+  },
+  {
+    id: 13,
+    name: "Camiseta Real Madrid",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Camisetas",
+    price: 160,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+  },
+  {
+    id: 14,
+    name: "Short Nike",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Shorts",
+    price: 160,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
+  },
+  {
+    id: 15,
+    name: "Tennis Puma",
+    sizes: ["S", "M", "L", "XL"],
+    category: "Tennis",
+    price: 160,
+    quantity: 1,
+    imageSrc:
+      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
+  },
+];
   
 
 
