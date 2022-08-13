f7308b04cb1370ae7645dd288e5cccb15bd7077c
Andres2310 <and.23108@gmail.com>
2022-08-12 22:10:49 -0400

    mongo connection

--- index.js
+++ index.js
@@ -1,162 +1,141 @@
+require('dotenv').config();
+
+
 const express = require("express");
+const mongoose=require("mongoose");
+const mongoDBenv=process.env.DATABASE_URL;
+const Model = require('./model/model');
+
+mongoose.connect(mongoDBenv);
+const database=mongoose.connection;
+
 const bodyParser=require("body-parser")
+
 const app = express();
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.json());
+
+database.on("error",(error)=>{
+  console.log(error);
+});
+
+database.once('connected',()=>{
+  console.log("Conectado a mongoDB")
+});
 
 const producto = [
-  {
-    id: 1,
-    name: "Hoodie Anime",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Sudaderas",
-    price: 180,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1660249137/e-commerce-x/hoddie_anime_swzi9p.webp",
-  },
-  {
-    id: 2,
-    name: "Sudadera Nike",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Sudaderas",
-    price: 100,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1660249137/e-commerce-x/hoddie_anime_swzi9p.webp",
-  },
-  {
-    id: 3,
-    name: "Hoodie Anime",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Sudaderas",
-    price: 150,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1660249137/e-commerce-x/hoddie_anime_swzi9p.webp",
-  },
-  {
-    id: 4,
-    name: "Short Adidas",
-    price: 160,
-    sizes: ["S", "M", "L", "XL"],
-    category: "Shorts",
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
-  },
-  {
-    id: 5,
-    name: "Camiseta Puma",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Camisetas",
-    price: 180,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
-  },
-  {
-    id: 6,
-    name: "Sudadera Nike",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Sudaderas",
-    price: 100,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
-  },
-  {
-    id: 7,
-    name: "Pantalón Azul",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Pantalones",
-    price: 150,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-  },
-  {
-    id: 8,
-    name: "Short Adidas",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Shorts",
-    price: 160,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
-  },
-  {
-    id: 9,
-    name: "Camiseta Puma",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Camisetas",
-    price: 180,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-  },
-  {
-    id: 10,
-    name: "Sudadera Nike",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Sudaderas",
-    price: 100,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-  },
-  {
-    id: 11,
-    name: "Tennis Nike",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Tennis",
-    price: 150,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-  },
-  {
-    id: 12,
-    name: "Short Adidas",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Shorts",
-    price: 160,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
-  },
-  {
-    id: 13,
-    name: "Camiseta Real Madrid",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Camisetas",
-    price: 160,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
-  },
-  {
-    id: 14,
-    name: "Short Nike",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Shorts",
-    price: 160,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
-  },
-  {
-    id: 15,
-    name: "Tennis Puma",
-    sizes: ["S", "M", "L", "XL"],
-    category: "Tennis",
-    price: 160,
-    quantity: 1,
-    imageSrc:
-      "https://res.cloudinary.com/daobmfotr/image/upload/v1659041231/e-commerce-x/madrid_pehnxn.webp",
-  },
-];
+    {
+      id: 1,
+      name: "Camiseta Puma",
+      colors: ["red", "gray", "black", "blue "],
+      price: 180,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 2,
+      name: "Sudadera Nike",
+      colors: ["red", "gray", "black", "blue "],
+      price: 100,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 3,
+      name: "Polera City",
+      colors: ["red", "gray", "black", "blue "],
+      price: 150,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 4,
+      name: "Short Adidas",
+      price: 160,
+      colors: ["red", "gray", "black", "blue "],
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 5,
+      name: "Camiseta Puma",
+      colors: ["red", "gray", "black", "blue "],
+      price: 180,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 6,
+      name: "Sudadera Nike",
+      colors: ["red", "gray", "black", "blue "],
+      price: 100,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 7,
+      name: "Polera City",
+      colors: ["red", "gray", "black", "blue "],
+      price: 150,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 8,
+      name: "Short Adidas",
+      colors: ["red", "gray", "black", "blue "],
+      price: 160,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 9,
+      name: "Camiseta Puma",
+      colors: ["red", "gray", "black", "blue "],
+      price: 180,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 10,
+      name: "Sudadera Nike",
+      colors: ["red", "gray", "black", "blue "],
+      price: 100,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 11,
+      name: "Polera City",
+      colors: ["red", "gray", "black", "blue "],
+      price: 150,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    },
+    {
+      id: 12,
+      name: "Short Adidas",
+      colors: ["red", "gray", "black", "blue "],
+      price: 160,
+      quantity: 1,
+      imageSrc:
+        "https://res.cloudinary.com/daobmfotr/image/upload/v1657863864/e-commerce-x/argentina_iltkio.webp",
+    }
+  ];
   
 
 
@@ -176,7 +155,7 @@ const producto = [
    });
 
 
-   app.get('/producto', function (req, res) {
+   app.get('/productos', function (req, res) {
 
     if(producto.id === '' || producto.nombre=== '') {
      respuesta = {
@@ -192,96 +171,28 @@ const producto = [
    });
 
 
-   app.post('/usuario', function (req, res) {
-    if(!req.body.nombre || !req.body.apellido) {
-     respuesta = {
-      error: true,
-      codigo: 502,
-      mensaje: 'El campo nombre y apellido son requeridos'
-     };
-    } else {
-     if(usuario.nombre !== '' || usuario.apellido !== '') {
-      respuesta = {
-       error: true,
-       codigo: 503,
-       mensaje: 'El usuario ya fue creado previamente'
-      };
-     } else {
-      usuario = {
-       nombre: req.body.nombre,
-       apellido: req.body.apellido
-      };
-      respuesta = {
-       error: false,
-       codigo: 200,
-       mensaje: 'Usuario creado',
-       respuesta: usuario
-      };
-     }
+   app.post('/postProducto', async (req, res) => {
+    const data = new Model({
+        id: req.body.id,
+        name: req.body.age,
+        sizes: req.body.sizes,
+        category: req.body.category,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        imageSrc: req.body.imageSrc
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
     }
-    
-    res.send(respuesta);
-   });
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+});
 
 
-   app.put('/usuario', function (req, res) {
-    if(!req.body.nombre || !req.body.apellido) {
-     respuesta = {
-      error: true,
-      codigo: 502,
-      mensaje: 'El campo nombre y apellido son requeridos'
-     };
-    } else {
-     if(usuario.nombre === '' || usuario.apellido === '') {
-      respuesta = {
-       error: true,
-       codigo: 501,
-       mensaje: 'El usuario no ha sido creado'
-      };
-     } else {
-      usuario = {
-       nombre: req.body.nombre,
-       apellido: req.body.apellido
-      };
-      respuesta = {
-       error: false,
-       codigo: 200,
-       mensaje: 'Usuario actualizado',
-       respuesta: usuario
-      };
-     }
-    }
-    
-    res.send(respuesta);
-   });
-   app.delete('/usuario', function (req, res) {
-    if(usuario.nombre === '' || usuario.apellido === '') {
-     respuesta = {
-      error: true,
-      codigo: 501,
-      mensaje: 'El usuario no ha sido creado'
-     };
-    } else {
-     respuesta = {
-      error: false,
-      codigo: 200,
-      mensaje: 'Usuario eliminado'
-     };
-     usuario = { 
-      nombre: '', 
-      apellido: '' 
-     };
-    }
-    res.send(respuesta);
-   });
-   app.use(function(req, res, next) {
-    respuesta = {
-     error: true, 
-     codigo: 404, 
-     mensaje: 'URL no encontrada'
-    };
-    res.status(404).send(respuesta);
-   });
+
 
 
 app.listen(3000, () => {
